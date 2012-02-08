35301b3383962409904d9bf4f15a0ed36250d4e4
s20dan <s20dan@hotmail.co.uk>
2012-02-08 22:21:01 +0000

    Borrow some parts from Ae's hdr branch, his values work well. Tweak some other values.

--- data/shaders/postprocessLuminance.frag.glsl
+++ data/shaders/postprocessLuminance.frag.glsl
@@ -4,7 +4,17 @@ varying vec2 texcoord;
 
 void main(void)
 {
+  #define threshold log(0.1)
 	const float delta = 0.001;
 	vec3 col = vec3(texture2DRect(fboTex, texcoord.st));
-	gl_FragColor = vec4(log(delta + dot(col, vec3(0.299,0.587,0.114))));
+	gl_FragColor = vec4(log(delta + dot(col, vec3(0.2126,0.7152,0.0722)))); //vec3(0.299,0.587,0.114)
+	gl_FragColor.g = step(threshold,gl_FragColor.g);
 }
+
+//{
+//	#define threshold log(0.1)
+//	const float delta = 0.001;//0.299,0.587,0.114
+//	vec3 col = vec3(texture2DRect(fboTex, texcoord.st));
+//	gl_FragColor = vec4(log(delta + dot(col, vec3(0.2126, 0.7152, 0.0722))));
+//	gl_FragColor.g = step(threshold,gl_FragColor.g);
+//}
